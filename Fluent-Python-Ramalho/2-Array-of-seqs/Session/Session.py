6b17c928e6f210dd8bcf773194eadad7c9dfe78f
Brian Tsu <tsu.brian.v@gmail.com>
2021-10-26 23:13:40 -0700

    local Sessions import seshTrack

---
+++ Fluent-Python-Ramalho/2-Array-of-seqs/Session/Session.py
@@ -0,0 +1,16 @@
+from Affirmations import affirm
+from datetime import datetime
+
+@affirm() # prints an affirmation to stdout 20% of the time this function is run
+def seshTrack(recordname): #take any type for recordname
+    '''
+    Prints the current time in dd/mm/YY H:M:S format
+    '''
+    now = datetime.now() # datetime object w/ current date and time
+    dt_string = now.strftime("%m/%d/%Y %H:%M:%S") #store date string in explicit format
+    print(f"{recordname}", dt_string) #recordname as string to tag the date
+
+if __name__ == "__main__":
+    pass
+else:
+    seshTrack("Starting session:")
